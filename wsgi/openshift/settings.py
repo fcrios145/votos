5b01356cf3032df4c312e367441a2c43902fa3c9
fcrios145 <fcrios145@hotmail.com>
2014-05-26 23:29:38 -0500

    static

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -153,11 +153,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 # Social AUTH
