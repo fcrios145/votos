b5c35e2f160f306c9c93215fa704638a7083374c
fcrios145 <fcrios145@hotmail.com>
2014-05-29 23:10:56 -0500

    ada

--- wsgi/openshift/apps/voto/views.py
+++ wsgi/openshift/apps/voto/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
 from django.shortcuts import render_to_response, redirect
 
 # Create your views here.
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView, ListView
 from django.contrib.auth import logout
 from .forms import FormParticipante
@@ -29,17 +31,22 @@ class Logout(TemplateView):
         return render_to_response('home/home.html')
         # return redirect('https://accounts.google.com/Logout?hl=es&continue=https://www.google.com.mx/')
 
+
 class Votar(ListView):
     template_name = 'home/votar.html'
     model = Participante
     context_object_name = 'participantes'
-    def get(self, request, *args, **kwargs):
-        participantes = Participante.objects.all()
-        if request.user.is_authenticated():
-            print "logiead"
-            return render_to_response('home/votar.html', {'participantes': participantes})
-        else:
-            print "no logueado"
-            return render_to_response('home/home.html')
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(Votar, self).dispatch(*args, **kwargs)
+    # def get(self, request, *args, **kwargs):
+    #     participantes = Participante.objects.all()
+    #     if request.user.is_authenticated():
+    #         print "logiead"
+    #         return render_to_response('home/votar.html', {'participantes': participantes})
+    #     else:
+    #         print "no logueado"
+    #         return render_to_response('home/home.html')
 
 
