0358ed538e17eba2a884301548abe021ce27bd36
fcrios145 <fcrios145@hotmail.com>
2014-05-30 20:57:29 -0500

    muchos cambios

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -166,7 +166,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 if ON_OPENSHIFT:
-    MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', 'media')
+    MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', '')
 else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
